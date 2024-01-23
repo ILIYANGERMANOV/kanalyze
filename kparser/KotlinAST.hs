3d0b7d22fd26c22e1cdf13847e86b6a6777223df
iliyangermanov <iliyan.germanov971@gmail.com>
2024-01-24 00:54:03 +0200

    WIP: Kotlin AST

--- kparser/KotlinAST.hs
+++ kparser/KotlinAST.hs
@@ -3,48 +3,49 @@ module KotlinAST where
 data KVisibility = KPublic | KPrivate | KInternal | KProtected
   deriving (Show, Eq)
 
+type ClassRef = String
+
 data KType
   = KUnit
   | KNothing
-  | KType
-      { _ktypeName :: String,
-        _nullable :: Bool
-      }
+  | KInt Int
+  | KLong Int
+  | KDouble Double
+  | KFloat Float
+  | KString String
+  | KBoolean Bool
+  | KClassRef ClassRef
+  | KLambda KSignature
+  | KNullable KType
+  deriving (Show, Eq)
+
+data KSignature = KSignature
+  { _sigAnnotations :: [KAnnotationValue],
+    _sigSuspending :: Bool,
+    _sigReceivers :: [KType],
+    _sigParams :: [KParamDef],
+    _sigOutput :: KType
+  }
   deriving (Show, Eq)
 
 data KParamDef = KParamDef
-  { _paramDefAnnotations :: [KAnnotation],
+  { _paramDefAnnotations :: [KAnnotationValue],
     _paramDefName :: String,
     _paramDefType :: KType,
     _paramDefaultValue :: Maybe KExpresssion
   }
   deriving (Show, Eq)
 
-data KParam = KParam
+data KParamValue = KParamValue
   { _paramName :: String,
     _paramValue :: KExpresssion
   }
   deriving (Show, Eq)
 
-data KAnnotation = KAnnotation
+data KAnnotationValue = KAnnotationValue
   { _annotationName :: String,
-    _annotationParams :: [KParam]
+    _annotationParams :: [KParamValue]
   }
-  deriving (Show, Eq)
-
-data KClass = KDataClass
-  { _dataClassAnnotations :: [KAnnotation],
-    _dataClassName :: String
-  }
-  deriving (Show, Eq)
-
-data KLiteral
-  = KString
-      { _kStringValue :: String
-      }
-  | KInt
-      { _kIntValue :: Int
-      }
   deriving (Show, Eq)
 
 data KExpresssion
@@ -57,16 +58,8 @@ data KExpresssion
       }
   deriving (Show, Eq)
 
-data KSignature = KSignature
-  { _sigReceivers :: [KType],
-    _sigParams :: [KParamDef],
-    _sigOutput :: KType
-  }
-  deriving (Show, Eq)
-
-data KFun = KFun
-  { _funAnnotations :: [KAnnotation],
-    _funVisibility :: KVisibility,
+data KFunDef = KFunDef
+  { _funVisibility :: KVisibility,
     _funName :: String,
     _funSignature :: KSignature,
     _funBody :: [KExpresssion]
